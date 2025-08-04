71bb7399b9a131425762d59d06f3432515514307
p-lizhengyang15 <p-lizhengyang15@xiaomi.com>
2025-08-04 10:59:02 +0800

    fix： 解决kimi api适配问题

--- tool/getModelList.py
+++ tool/getModelList.py
@@ -1,7 +1,7 @@
 from openai import OpenAI
  
 client = OpenAI(
-    api_key = "sk-sfaBCLrIZ3PZOuY05EJ1V3P3gw5LFNUPfTVET1K3sPFl7lXr",
+    api_key = "sk-FyCX0MpCwsjEVWguJHnag0P26UTGDJz3CPAA2dlQZo3IbdFA",
     base_url = "https://api.moonshot.cn/v1",
 )
  
